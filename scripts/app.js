f7b5b0954ee2e6c6ae48bfc4cd55b98ca5586b06
James Nocentini <james.nocentini@gmail.com>
2014-04-22 13:48:19 +0100

    coomit

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,8 @@ angular.module('testApp', [])
 
   .factory('myPouch', function() {
     var mydb = new PouchDB('ng-pouch');
-    PouchDB.replicate('http://107.170.28.114:5984/ng-db', 'ng-pouch', {continuous: true});
-//    PouchDB.replicate('ng-pouch', 'http://107.170.28.114:5984/ng-db', {continuous: true});
+    PouchDB.replicate('http://127.0.0.1:5984/ng-db', 'ng-pouch', {continuous: true});
+    PouchDB.replicate('ng-pouch', 'http://127.0.0.1:5984/ng-db', {continuous: true});
     return mydb;
   })
 
